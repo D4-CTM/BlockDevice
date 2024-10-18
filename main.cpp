0a5e6e1dd7eb2be6bd9ab698bd9ad62752ff2437
D4-CTM <josuedelcid325@gmail.com>
2024-10-18 12:15:38 -0600

    partition tools initial data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+struct test {
+    unsigned int size;
+    unsigned int cant;
+};
+
 int main() {
+    test* tp = new test();    
+    test t = *tp;
+
+    std::cout << &tp << '\n';
+    std::cout << &t << '\n';
 
     return 0;
 }
--- tools/Partition.hpp
+++ tools/Partition.hpp
@@ -1,31 +1,62 @@
-#ifndef __PARTITION__
-#define __PARTITION__
+#ifndef __PARTIONER__
+#define __PARTIONER__
+#include <fstream>
 
-class Partition {
+class Disk_partioner {
+//Strucs and subclasses used to create the 'disk partition' simulation file
 private:
-    struct SuperBlock {
-        unsigned char partition_name[20];
+    struct Superblock {
         unsigned int blocks_size;
         unsigned int blocks_cant;
     };
-    SuperBlock* SB;
-public:
 
-    Partition(const unsigned char name[20], unsigned int size, unsigned int cant) {
-        SB = new SuperBlock();
-        SB->blocks_size = size;
-        SB->blocks_cant = cant;
-        for (int i = 0; i < 20; i++) {
-            SB->partition_name[i] = name[i];
+    struct Block {
+        void setBlockData(int bytes_cant) {
+            bytes = new unsigned char[bytes_cant];
+            free = true;
         }
 
+        unsigned char* bytes;
+        bool free;
+    };
+
+//private variables
+private:
+    Superblock* sb;
+    Block* blocks;
+//main methods
+public:
+
+    Disk_partioner(unsigned int blocks_cant, unsigned int blocks_size) 
+    { setPartitionConfiguration(blocks_cant, blocks_size); }
+
+    //set the data of the superblock to indicate how many blocks and how many they are gonna weight
+    void setPartitionConfiguration(unsigned int blocks_cant, unsigned int blocks_size) {
+        sb = new Superblock();
+        sb->blocks_cant = blocks_cant;
+        sb->blocks_size = blocks_size;
+
+        blocks = new Block[blocks_cant];
+        
     }
 
+    void createPartition(const char* fileName) {
+        if (sb == nullptr) {
+            throw std::runtime_error("The superblock data is not set!");
+        }
 
+        std::ofstream fileWriter(fileName, std::ios::binary);
+        if (fileWriter.is_open()) {
+            fileWriter.write(reinterpret_cast<char *>(&sb), sizeof(Superblock));
+            
+            fileWriter.close();
+        }
+    }
 
-    const SuperBlock getSuperBlock()
-    { return *SB; }
-
+//Private functions used within the code
+private:
+    inline int calculate_partition_size(int blocks, int blocks_size) 
+    { return blocks * blocks_size; }
 };
 
-#endif //__PARTITION__
+#endif //__PARTIONER__
